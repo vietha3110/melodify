2e00290bb6e0125e56bdf465da7304e8244b6d45
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-12-21 09:54:55 -0800

    Fixed login form.

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -38,7 +38,6 @@ const LoginForm = () => {
         ))}
       </div>
       <div>
-        <label htmlFor='email'>Email</label>
         <input
           name='email'
           type='text'
@@ -48,7 +47,6 @@ const LoginForm = () => {
         />
       </div>
       <div>
-        <label htmlFor='password'>Password</label>
         <input
           name='password'
           type='password'
