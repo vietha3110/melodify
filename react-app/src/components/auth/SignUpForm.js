c2ef898763cde62525f38717a878e06be11e281e
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-12-21 09:50:00 -0800

    Fixed signup form.

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -5,42 +5,21 @@ import { signUp } from '../../store/session';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('')
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
-    e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
-      if (data) {
-        setErrors(data)
-      }
+    e.preventDefault(); 
+    const data = await dispatch(signUp( email, password));
+    if (data) {
+      setErrors(data)
     }
   };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
-
-  if (user) {
-    return <Redirect to='/' />;
-  }
 
   return (
     <form onSubmit={onSignUp}>
@@ -50,41 +29,43 @@ const SignUpForm = () => {
         ))}
       </div>
       <div>
-        <label>User Name</label>
         <input
           type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
+          name='firstname'
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder='First Name'
+          value={firstName}
         ></input>
       </div>
       <div>
-        <label>Email</label>
         <input
           type='text'
+          name='lastname'
+          onChange={e => setLastName(e.target.value)}
+          placeholder='Last Name'
+          value={lastName}
+        ></input>
+      </div>
+      <div>
+        <input
+          type='email'
           name='email'
-          onChange={updateEmail}
+          onChange={e => setEmail(e.target.value)}
+          placeholder='Email'
           value={email}
-        ></input>
+        />
       </div>
       <div>
-        <label>Password</label>
         <input
           type='password'
           name='password'
-          onChange={updatePassword}
+          onChange={e => setPassword(e.target.value)}
           value={password}
-        ></input>
+          placeholder='Password'
+        />
       </div>
       <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
+        <span>You acknowledge that you agree to the Melodify Services Terms & Conditions when you select Sign Up.</span>
       </div>
       <button type='submit'>Sign Up</button>
     </form>
