2c72dbb6bf3408dc6da8e3f128c03fcd7de833c7
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-12-25 21:11:01 -0800

    Added song_routes/get all & upload song.

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, make_response
 from flask_login import login_required, current_user
 from ..models import db, Song, File
+from ..forms import UploadSongForm
 
 
 song_routes = Blueprint('songs', __name__) 
@@ -24,3 +25,28 @@ def user_songs():
 def upload_song(): 
     current_user_info = current_user.to_dict()
     current_user_id = current_user_info['id']
+    form = UploadSongForm()
+    audio_file = form['audioFile']
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate(): 
+        try: 
+            new_file = File(
+                file_song = audio_file.read()
+            )
+            db.session.add(new_file)
+            db.session.commit()
+            new_song = Song(
+                name = form['name'], 
+                artist_name = form['artist_name'],
+                genre = form['genre'],
+                length = form['length'],
+                user_id = current_user_id,
+                file_id = new_file.id 
+            )
+            db.session.add(new_song)
+            db.session.commit()
+            return new_song.to_dict(), 201
+        except Exception:
+            return {'error': 'there is an error, please try again'}, 500
+    if form.errors: 
+        return {'error': form.erros}, 400
