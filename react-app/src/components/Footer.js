93766e8a12f66902a06fb2796a32c7ca27520987
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-01-04 09:49:00 -0800

    Added basic css to style footer.

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -4,17 +4,14 @@ const Footer = () => {
         <div className="footer-main"> 
             <div className="footer-label">
                 <span>
-                    © 2022 melodify.
-                </span>
-                <span>
-                    Developed by Ha Nguyen
+                    © 2022 melodify - Developed by Ha Nguyen
                 </span>
             </div>
             <div className="footer-link">
-                <a href="https://www.linkedin.com/in/havietng/">
+                <a href="https://www.linkedin.com/in/havietng/" style={{margin: "2px 2px 0px 2px", textDecoration:"#000"}}>
                     Linkedin
                 </a>
-                <a href="https://github.com/vietha3110">
+                <a href="https://github.com/vietha3110" style={{margin: "2px 2px 0px 2px", textDecoration:"#000"}}>
                     Github
                 </a>
             </div>
