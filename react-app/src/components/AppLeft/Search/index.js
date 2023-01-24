7f918dc3882259b5af5da02082269bfe3d5b6f7c
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-01-25 01:09:02 +0700

    Added css for search bar.

--- react-app/src/components/AppLeft/Search/index.js
+++ react-app/src/components/AppLeft/Search/index.js
@@ -9,34 +9,39 @@ const Search = () => {
         }
         const searchInput = input.toLowerCase();
         const result = fetch(`/api/songs/search/${searchInput}`)
-        .then(res => res.json())
-        .then(res => {
-            setSearchResult(res);
-        })
-        .catch(e => console.log(e));
+            .then(res => res.json())
+            .then(res => {
+                console.log('resultsong', res)
+                setSearchResult(res);
+            })
+            .catch(e => console.log(e));
     }, [input])
     
     return (
-        <div className='app-left-search' style={{cursor:"default"}}>
-            <div>
-                <span className="material-symbols-outlined" style={{padding: 8, cursor:"default"}}>
-                    search
-                </span>
-                <input
-                    className="search-input"
-                    placeholder="Search"
-                    value={input}
-                    onChange={e => setInput(e.target.value)}
-                />
-            </div>
+        <div className='app-left-search' style={{ cursor: "default" }}>
+            <span className="material-symbols-outlined" style={{ padding: 8, cursor: "default" }}>
+                search
+            </span>
+            <input
+                className="search-input"
+                placeholder="Search"
+                value={input}
+                onChange={e => setInput(e.target.value)}
+            />
             {
-                searchResult.length > 0 && 
-                    searchResult.map(ele => (
-                        <div style={{backgroundColor:"red"}}>
-                            {ele.song}
-                        </div>
-                    ))
-            }
+                searchResult.length > 0 &&
+                < div className='search-result' >
+                    {
+                        searchResult.map(ele => (
+                            <div>
+                                <div className="search-result-song">
+                                    {ele.song}
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+}
             
         </div>
     )
