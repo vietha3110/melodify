553df47b1d182c42f94735f31b7be2b88a3cfb5a
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-01-25 02:20:38 +0700

    Created Hireme banner.

---
+++ react-app/src/components/Hirebanner.js
@@ -0,0 +1,21 @@
+const HireMe = () => {
+    return (
+        <div className="hireme">
+            <div className="hireme-text">
+                <div className="hireme-text-headline">
+                    Let Ha help you hit all the right notes with her Full Stack Development skills
+                </div>
+                <div className="hireme-text-subhead">
+                    With her Frontend and Backend proficiency, she'll make sure your platform is the 'Shape of You' in the digital world.
+                </div>
+            </div>
+            <div className="hireme-btn">
+                <button>
+                    Hire Ha! 
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default HireMe;
