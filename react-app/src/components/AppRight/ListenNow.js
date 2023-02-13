558c6af8fe0e8d47b71fad2cf9d4cfc3251cd62e
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-02-13 08:53:46 -0800

    Draft listen now page for non-login.

---
+++ react-app/src/components/AppRight/ListenNow.js
@@ -0,0 +1,21 @@
+const ListenNow = () => {
+    return (
+        <div className="listennow-container">
+            <div className="listennow-logo">
+                <span>melodify</span>
+            </div>
+            <div className="listennow-header">
+                Discover new music everyday.
+            </div>
+            <div className="listennow-subhead">
+                Play and share your favorite songs on melodify.
+            </div>
+            <div className="listennow-button">
+                <button>Try it free</button>
+            </div>
+            <div className="listennow-info">
+
+            </div>
+        </div>
+    )
+}
